742759c5434f7e360d5368b7b3ca736dcc71fde5
Sean Stacey <kumori@ryougi.com>
2023-12-04 14:38:55 -0500

    Slight optimization, don't have to create a new day obj for each file

--- src/main.rs
+++ src/main.rs
@@ -18,13 +18,14 @@ fn main() {
     match args.day {
         Some(x) => {
             // Run every file that starts with dayX in input folder
+            let day = days::get_day(x);
             for file in files {
                 let file = file.unwrap();
                 let path = file.path();
                 let path_str = path.to_str().unwrap();
                 let m = format!("input/day{}", x.to_string());
                 if path_str.starts_with(&m) {
-                    days::get_day(x).main(path_str);
+                    day.main(path_str);
                 }
             }
         }
